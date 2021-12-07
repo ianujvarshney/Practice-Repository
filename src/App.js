b11e4cea798f3b0696b1abe49da0a2b64fa51230
ianujvarshney <anujkk19111997@gmail.com.com>
2021-12-07 18:13:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import backgrou from './backgrou.jpg';
+import logo from './images/logo.svg';
+import React from 'react';
+import backgrou from './images/backgrou.svg';
+import background from './images/background.svg';
 import './App.css';
+// import Apple from './components/Apple';
+import './app.scss';
+/* import { Navbar, Nav, NavDropdown, Form, Button, Container, FormControl } from 'react-bootstrap'; */
 function App() {
   return (
     <>
       <section>
         <div className="box">
           <div className="box-items">
-            <img src={backgrou} />
+            <img src={background} />
           </div>
-          <h1 className="heading"><img src={logo}></img></h1>
+          <h1 className="icon"><img src={logo}></img></h1>
+          <h1 className="icon-2"><img src={backgrou}></img></h1>
           <h1 className="text-center new-1">Sign Up as..</h1>
           <div className="card-group">
             <button> <a href="#"> {"< Back"}</a></button>
