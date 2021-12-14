5a0179292b0d8a03b391aaff1f4eba0772b400ae
ianujvarshney <anujkk19111997@gmail.com>
2021-12-14 18:12:15 +0530

    intial commit-1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Page03 from './Pages/Page03';
 import Page04 from './Pages/Page04';
 import Page05 from './Pages/Page05';
 import Page06 from './Pages/Page06';
+import Page07 from './Pages/Page07';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="Page04" element={<Page04 />} />
           <Route path="Page05" element={<Page05 />} />
           <Route path="Page06" element={<Page06 />} />
+          <Route path="Page07" element={<Page07 />} />
         </Routes>
       </Router>
     </>
---
+++ src/Pages/Page07.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+import logo_1 from '../images/image-Logo.svg';
+import logo_2 from '../images/Subtract.svg';
+import logo_3 from '../images/Ellipse_8.svg';
+import { Link } from "react-router-dom";
+import '../Pagescss/Page07.css';
+import { Navbar, NavDropdown, Container } from 'react-bootstrap';
+
+function Page05() {
+    return (
+        <>
+
+            <div className="container-fluid" id="contain">
+                <div className="row">
+                    <Navbar className="Nav-bar" bg="light" variant="light" fixed="top" expand="lg">
+                        <Container fluid>
+                            <Navbar.Brand href="#" id="navbar-image">
+                                <Link to="/"><img id="img_1" height="100px" width="70px" src={logo} /></Link>
+                                <img id="img_2" src={logo_1} />
+                                <h2>DASHBOARD</h2>
+                            </Navbar.Brand>
+                            <Navbar.Brand className="ms-auto pe-md-5 navbar-nav">
+                                <h6>Welcome back, George</h6>
+                                <img height="100px" width="40px" src={logo_2} />
+                            </Navbar.Brand>
+                            <Navbar.Toggle class="navbar-toggle" aria-controls="navbarScroll" />
+                            <NavDropdown title="" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Container>
+                    </Navbar>
+                </div>
+                <div className="row" >
+                    <div className="col-4 section_f1">
+                        <h1>My Schedule</h1>
+                    </div>
+                    <div className="col-8 section_f2">
+                        <h1>My Coaches</h1>
+                        <div className="row" id="row-data">
+                            <header>Add Your Coach's Code
+                                <i class="ms-auto fas fa-times"></i>
+                            </header>
+                            <div className="col-5" id="items-1">
+                                <img id="image-data-1" height="310px" width="120px" src={logo_3} />
+                            </div>
+                            <div className="col-7" id="form-items">
+                                <form id="form-items" className="text-center">
+                                    <p >John cena</p>
+                                    <p >Coach Code:</p>
+                                    <Link id="Link-button-1" to="/">Countinue</Link>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+
+export default Page05;
